96e8a704a104375b5de718ee1c41b30d614f0a15
exnnwc <2buzezsn@gmail.com>
2016-01-04 13:52:44 -0800

    Resolved config issue between Linux and Windows boxes

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
 
 define ("SITE_NAME", "Real Life Achievements");
-define ("SITE_ROOT", "http://" . $_SERVER['SERVER_NAME'] . "/rla"   );
+if ($_SERVER['SERVER_NAME']=="rla.dev"){
+	define ("SITE_ROOT", "http://" . $_SERVER['SERVER_NAME']  );
+} else {
+	define ("SITE_ROOT", "http://" . $_SERVER['SERVER_NAME'] . "/rla"   );
+}
 define ("DB_HOST", "localhost");
 define ("DB_NAME", "rla");
 define ("DB_PWD", "");
