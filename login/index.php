aff13b901248a12e01ba4effdeae0ecc5a8bfece
exnnwc <2buzezsn@gmail.com>
2016-03-09 10:32:50 -0800

    Implemented basic login function

---
+++ login/index.php
@@ -0,0 +1,17 @@
+<?php require_once("../php/config.php"); ?>
+<html>
+<head>
+    <style>
+        .login-caption{
+            width:90px;
+            float:left;
+        }
+    </style>
+    <script src="<?php echo SITE_ROOT; ?>/js/jquery-2.1.4.min.js"></script>
+    <script src="<?php echo SITE_ROOT; ?>/js/user.js"></script>
+    <script src="<?php echo SITE_ROOT; ?>/js/login.js"></script>
+</head>
+<body>
+    <?php require_once("login.htm"); ?>
+</body></html>
+
